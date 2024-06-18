1a8268dad717b46d106fa070580f520db01480ef
Bugi Goertz <burkhard.goertz@shapefruit.de>
2024-06-18 17:57:08 +0200

    WIP

--- lua/bugi/lazy.lua
+++ lua/bugi/lazy.lua
@@ -11,4 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("bugi.plugins")
+require("lazy").setup({
+  { import = "bugi.plugins"},
+  { import = "bugi.plugins.lsp"}
+})
---
+++ lua/bugi/plugins/lsp/mason.lua
@@ -0,0 +1,24 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  -- add opts for lazy to mark this plugin for loading
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+
+
+    mason.setup()
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+        "emmet_ls",
+        "intelephense",
+    }})
+
+  end
+}
