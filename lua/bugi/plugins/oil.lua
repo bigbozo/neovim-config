4f34fcf533d17327ce131454981fab576d150011
Bugi Goertz <bugi@joboland.de>
2024-06-18 19:44:24 +0200

    configured oil.nvim

--- lua/bugi/plugins/oil.lua
+++ lua/bugi/plugins/oil.lua
@@ -2,7 +2,14 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    skip_confirm_for_simple_edits = true,
+    columns = {
+      "icon",
+      "size",
+      "mtime"
+    }
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
