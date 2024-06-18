708f80224c0241226f2ebe77760bf28d235c654f
Bugi Goertz <bugi@joboland.de>
2024-06-18 19:53:24 +0200

    moved vim-options to options.lua

---
+++ lua/bugi/core/options.lua
@@ -0,0 +1,13 @@
+-- General vim-settings
+local opts = vim.opt
+
+opts.tabstop=2
+opts.shiftwidth=2 -- was ist das? keine echte Ahnung
+opts.foldmethod="indent" -- Folding auf Basis der Code-Einrueckung
+opts.foldcolumn="1" -- zeige die +/- Spalte an
+opts.foldlevel=3
+opts.number=true -- zeige Zeilennummern an
+opts.expandtab=true  -- expandiere <Tab> zu Leerzeichen
+opts.smartindent=true -- was ist das? keine echte Ahnung
+
+
