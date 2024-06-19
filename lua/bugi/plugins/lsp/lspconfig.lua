1ee41099bc09ca63842ec0d3068cc106092dcb08
Bugi Goertz <bugi@joboland.de>
2024-06-19 20:06:32 +0200

    finalize lsp-config

--- lua/bugi/plugins/lsp/lspconfig.lua
+++ lua/bugi/plugins/lsp/lspconfig.lua
@@ -59,6 +59,46 @@ return {
       end,
     })
 
+local mason_lspconfig = require('mason-lspconfig')
+local lspconfig = require('lspconfig')
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+-- used to enable autocompletion (assign to every lsp server config)
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+mason_lspconfig.setup_handlers({
+  -- default handler for installed servers
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+    })
+  end,
+  ["emmet_ls"] = function()
+    -- configure emmet language server
+    lspconfig["emmet_ls"].setup({
+      capabilities = capabilities,
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    })
+  end,
+  ["lua_ls"] = function()
+    -- configure lua server (with special settings)
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    })
+  end,
+})
+
     -- Change the Diagnostic symbols in the sign column (gutter)
     -- (not in youtube nvim video)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
--- lua/bugi/plugins/lsp/mason.lua
+++ lua/bugi/plugins/lsp/mason.lua
@@ -23,14 +23,13 @@ return {
 
     vim.api.nvim_create_autocmd({'BufNew', 'BufEnter'}, {
       -- When run for all vim-lua files it throws tons of warning for global vim
-      pattern = { '*.p8', '*/carts/*.lua' },
+      pattern = { '*.p8' },
       callback = function(args)
         vim.lsp.start({
             name = 'pico8-ls',
             cmd = { 'pico8-ls', '--stdio' },
             root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
             -- Setup your keybinds in the on_attach function
-            on_attach = on_attach,
         })
       end
     })
