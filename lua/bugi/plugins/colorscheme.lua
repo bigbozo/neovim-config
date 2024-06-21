275e1c342db8895393a670cabb30548a116c9fc8
Bugi Goertz <bugi@joboland.de>
2024-06-21 23:42:10 +0200

    Installed gruvbox-material colorscheme
    I have no clue how to install the light variant
    
    Changelog: added

--- lua/bugi/plugins/colorscheme.lua
+++ lua/bugi/plugins/colorscheme.lua
@@ -1,8 +1,35 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd[[colorscheme tokyonight]]
-  end
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd[[colorscheme tokyonight]]
+    end
+  },{
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    opts = {-- values shown are defaults and will be used if not provided
+      italics = true,             -- enable italics in general
+      contrast = "medium",        -- set contrast, can be any of "hard", "medium", "soft"
+      comments = {
+        italics = true,           -- enable italic comments
+      },
+      background = {
+        transparent = false,      -- set the background to transparent
+      },
+      float = {
+        force_background = false, -- force background on floats even when background.transparent is set
+        background_color = nil,   -- set color for float backgrounds. If nil, uses the default color set
+        -- by the color scheme
+      },
+      signs = {
+        highlight = true,         -- whether to highlight signs
+      },
+      customize = nil,            -- customize the theme in any way you desire, see below what this
+      -- configuration accepts
+    }
+  },
 }
