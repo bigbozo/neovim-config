0e9c58fd49adec385cfdf3da23d804669d507e99
Bugi Goertz <bugi@joboland.de>
2024-06-24 23:04:53 +0200

    Removed pico8-ls
    There is no need for a special language-server. Opted for working with
    an pico8.lua-helperfile, providing syntax and going forward with the
    lua-lsp.

--- lua/bugi/plugins/lsp/mason.lua
+++ lua/bugi/plugins/lsp/mason.lua
@@ -18,20 +18,7 @@ return {
         "lua_ls",
         "emmet_ls",
         "intelephense",
-        "pico8_ls"
     }})
 
-    vim.api.nvim_create_autocmd({'BufNew', 'BufEnter'}, {
-      -- When run for all vim-lua files it throws tons of warning for global vim
-      pattern = { '*.p8' },
-      callback = function(args)
-        vim.lsp.start({
-            name = 'pico8-ls',
-            cmd = { 'pico8-ls', '--stdio' },
-            root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
-            -- Setup your keybinds in the on_attach function
-        })
-      end
-    })
   end
 }
